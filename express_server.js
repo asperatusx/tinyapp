ae935b1e2ea35c9873ff312fccc9c6124f13934e
Andrew <andrewlang.p@gmail.com>
2024-10-29 20:15:34 -0400

    add ejs template to enter new url and get request to render it

--- express_server.js
+++ express_server.js
@@ -18,6 +18,10 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 })
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
   res.render('urls_show', templateVars);
@@ -25,19 +29,6 @@ app.get('/urls/:id', (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
 });
 
 app.listen(PORT, () => {
